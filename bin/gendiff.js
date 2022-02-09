7302bafcc6e228a97e0fbe5e92034312782e4561
Albert.ES <art.bertes@gmail.com>
2022-02-09 16:36:16 +1000

    feat: создал функцию сравнения json данных

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+import { genDiff, getData } from '../src/genDiff.js';
 
-import { Command } from "commander";
 const program = new Command();
 
 program
-  .version('0.1')
+  .version('0.1', '-v, --vers', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
-
-program.command('genDiff')  
-  .option('-V', '--version', 'output the version number')
-  .option('-h', '--help', 'output usage information');
-
+  .option('-f, --format [type] ', 'output format')
+  .arguments('<filepath1>  <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(getData(filepath1), getData(filepath2)));
+  });
 program.parse();
---
+++ src/genDiff.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const getPath = (pathStr) => path.resolve(process.cwd(), pathStr);
+
+export const getData = (pathStr) => {
+  const data = fs.readFileSync(getPath(pathStr), 'utf-8');
+  return JSON.parse(data);
+};
+
+export const genDiff = (data1, data2) => {
+  const unionD = _.union(_.keys(data1), _.keys(data2)).sort();
+
+  const copy1 = _.clone(data1);
+  const copy2 = _.clone(data2);
+
+  const newObj = unionD.reduce((acc, item) => {
+    if (copy1[item] === copy2[item]) {
+      acc[`  ${item}`] = copy1[item];
+    }
+    if (_.has(copy1, item) && !_.has(copy2, item)) {
+      acc[`- ${item}`] = copy1[item];
+    }
+    if (copy1[item] !== copy2[item] && _.has(copy2, item) && _.has(copy1, item)) {
+      acc[`- ${item}`] = copy1[item];
+    }
+    if (_.has(copy2, item) && copy2[item] !== copy1[item]) {
+      acc[`+ ${item}`] = copy2[item];
+    }
+    return acc;
+  }, {});
+  return newObj;
+};
+
+export default genDiff;
